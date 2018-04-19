8ba5a70d35e7c3bc8af5f3727ca0a4e2e73d7055
mkobilov <36609865+mkobilov@users.noreply.github.com>
2018-04-19 16:29:37 +0300

    Add files via upload

--- fmain.c
+++ fmain.c
@@ -1,61 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "jsonparser.h"
+int main(){
+	
+	FILE* file = fopen("test.txt","r");
+	
+	pHashMap map = ParseJSON(file);
+	pJsonElement res = GetFromHMap(map,"bla");
+	printf("bla = %s\n",(char*) res->value );
+	
+	pJsonElement jsonmap = (pJsonElement) calloc(1,sizeof(JsonElement));
+	jsonmap->type = JSON_MAP;
+	jsonmap->value = map;
+	DeleteJSON(jsonmap);
+	fclose(file);
+	
 
-//#include "list.h"
-#include "list_map.h"
-
-#ifdef DEBUG
-	#include <stdio.h>
-	#define DEBUG_PRINT(...) printf(__VA_ARGS__);
-#else
-	#define DEBUG_PRINT(...)
-#endif
-
-int main()
-{
-	DEBUG_PRINT("HELLO %d\n", 1);
-	
-	pMap map = CreateMap();
-		
-	AddToMap(map, "q", 4);
-	AddToMap(map, "w", 5);
-	AddToMap(map, "e", 6);
-	AddToMap(map, "2", 7);
-	AddToMap(map, "w", 7);
-	
-	printf("%d %d\n", IsInMap(map, "q"), GetFromMap(map, "q"));
-	printf("%d %d\n", IsInMap(map, "w"), GetFromMap(map, "w"));
-	printf("%d %d\n", IsInMap(map, "e"), GetFromMap(map, "e"));
-	
-	printf("%d %d\n", IsInMap(map, "r"), GetFromMap(map, "r"));
-		
-	ListIterator iter;
-	for(iter = GetBeginListIterator(map->data);
-		!IsListIteratorNULL(&iter);
-		ListIteratorNext(&iter))
-	{
-		pMapElement current = (pMapElement) GetListIteratorValue(&iter);
-		printf("=> %s %d\n", current->key, current->value);
-	}
-	
-	DeleteMap(&map);
-	
-	/*
-	// 
-	pList a = CreateList();
-	
-	PushBackToList(a, 4);
-	PushBackToList(a, 8);
-	PushBackToList(a, 12);
-	
-	pListIterator iter = CreateBeginListIterator(a);
-	while(iter)
-	{
-		printf("%d\n", GetListIteratorValue(iter));
-		ListIteratorNext(&iter);
-	}
-	
-	DeleteList(&a);*/
 	
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,10 +1,13 @@
 CC = gcc
 CFLAGS=-g
 
-all: jsonparser.o list.o myhashmap.o listiterator.o
-	$(CC) $(CFLAGS) jsonparser.o list.o myhashmap.o listiterator.o  -static -o test.exe
-main: jsonparser.c
-	$(CC) $(CFLAGS) -c jsonparser.c
+all: fmain.o jsonparserf.o list.o myhashmap.o listiterator.o
+	$(CC) $(CFLAGS) fmain.o jsonparserf.o list.o myhashmap.o listiterator.o  -static -o test.exe
+
+main:
+	$(CC) $(CFLAGS) -c fmain.c
+jsonparser: jsonparserf.c
+	$(CC) $(CFLAGS) -c jsonparserf.c
 list: list.c
 	$(CC) $(CFLAGS) -c list.c
 map: myhashmap.c
