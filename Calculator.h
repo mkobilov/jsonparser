7ff36b754a3f0ca83988b9d98f719795982a0bcc
mkobilov <36609865+mkobilov@users.noreply.github.com>
2018-04-24 20:30:23 +0300

    Delete Calculator.h

--- Calculator.h
+++
@@ -1,8 +0,0 @@
-#ifndef __CALCULATOR_H__
-#define __CALCULATOR_H__
-#include "jsonparser.h"
-
-int Calculator(const char* file_name);
-int Calculate(pJsonElement calc_elem);
-int Sum(const pJsonElement sum_elem);
-#endif
